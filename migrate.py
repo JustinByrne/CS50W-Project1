4d7dc2c779655d96bf639c341f7d3c60163bd4e5
JustinByrne <theskaterjesus@gmail.com>
2020-06-13 22:46:59 +0100

    Rebuilt tables to conform to project requirements

--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 import os
 
-
-from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,59 +10,65 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(os.getenv("DATABASE_URL"))
-meta = MetaData()
+db = scoped_session(sessionmaker(bind=engine))
 
 if not engine.has_table('authors'):
-    authors = Table(
-        'authors', meta,
-        Column('id', Integer, primary_key = True),
-        Column('name', String(255), nullable=False),
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime)
-    )
+    db.execute("\
+        CREATE TABLE authors (\
+            id SERIAL PRIMARY KEY,\
+            name VARCHAR NOT NULL,\
+            created_at TIMESTAMP,\
+            updated_at TIMESTAMP\
+        )\
+    ")
 
     print("The `authors` table has been created.")
 
 if not engine.has_table('books'):
-    books = Table(
-        'books', meta,
-        Column('id', Integer, primary_key = True),
-        Column('isbn', String(20), unique = True, nullable=False),
-        Column('title', String(100), nullable=False),
-        Column('author_id', Integer, ForeignKey("authors.id"), nullable=False),
-        Column('year', Integer, nullable=False),
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime)
-    )
+    db.execute("\
+        CREATE TABLE books (\
+            id SERIAL PRIMARY KEY,\
+            isbn VARCHAR NOT NULL,\
+            title VARCHAR NOT NULL,\
+            author_id INTEGER REFERENCES authors(id) NOT NULL,\
+            year INTEGER NOT NULL,\
+            created_at TIMESTAMP,\
+            updated_at TIMESTAMP,\
+            UNIQUE(isbn)\
+        )\
+    ")
 
     print("The `books` table has been created.")
 
 if not engine.has_table('users'):
-    users = Table(
-        'users', meta,
-        Column('id', Integer, primary_key = True),
-        Column('username', String(25), unique = True, nullable=False),
-        Column('password', String(255), nullable=False),
-        Column('forename', String(25), nullable=False),
-        Column('surname', String(25)),
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime)
-    )
+    db.execute("\
+        CREATE TABLE users (\
+            id SERIAL PRIMARY KEY,\
+            username VARCHAR NOT NULL,\
+            password VARCHAR NOT NULL,\
+            forename VARCHAR NOT NULL,\
+            surname VARCHAR,\
+            created_at TIMESTAMP,\
+            updated_at TIMESTAMP,\
+            UNIQUE(username)\
+        )\
+    ")
 
     print("The `users` table has been created.")
 
 if not engine.has_table('reviews'):
-    reviews = Table(
-        'reviews', meta,
-        Column('id', Integer, primary_key = True),
-        Column('user_id', Integer, ForeignKey("users.id"), nullable=False),
-        Column('book_id', Integer, ForeignKey("books.id"), nullable=False),
-        Column('rating', Integer, nullable=False),
-        Column('comment', String(255)),
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime)
-    )
+    db.execute("\
+        CREATE TABLE reviews (\
+            id SERIAL PRIMARY KEY,\
+            user_id INTEGER REFERENCES users(id) NOT NULL,\
+            book_id INTEGER REFERENCES books(id) NOT NULL,\
+            rating INTEGER NOT NULL,\
+            comment VARCHAR,\
+            created_at TIMESTAMP,\
+            updated_at TIMESTAMP\
+        )\
+    ")
 
     print("The `reviews` table has been created.")
 
-meta.create_all(engine)
+db.commit()
