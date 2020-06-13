4ff9839dc1b9cc48621be26656a94890f4a29889
JustinByrne <theskaterjesus@gmail.com>
2020-06-14 00:07:10 +0100

    finished import script

--- import.py
+++ import.py
@@ -12,8 +12,27 @@ db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     file = open("books.csv")
-    read = csv.reader(file)
+    reader = csv.reader(file)
+    
+    # skipping over first row
+    next(reader)
+    
+    for isbn, title, author, year in reader:
+        # importing Author
+        auth = db.execute("SELECT id FROM authors WHERE name = :name", {"name": author}).fetchone()
 
+        if auth is None:
+            db.execute("INSERT INTO authors (name, created_at, updated_at) VALUES (:name, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)",
+                {"name": author})
+            auth = db.execute("SELECT id FROM authors WHERE name = :name", {"name": author}).fetchone()
+
+        # importing books
+        db.execute("INSERT INTO books (isbn, title, author_id, year, created_at, updated_at) VALUES (:isbn, :title, :author_id, :year, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)",
+            {"isbn": isbn, "title": title, "author_id": auth.id, "year": year})
+        
+        print(f"Added {title} to the database")
+    
+    db.commit()
 
 if __name__ == "__main__":
     main()
