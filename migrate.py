fe50be8d24682460eaf9e8a6b7b0d3a979dcc89c
JustinByrne <theskaterjesus@gmail.com>
2020-06-14 00:06:36 +0100

    created a down func and set the main func

--- migrate.py
+++ migrate.py
@@ -12,63 +12,86 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-if not engine.has_table('authors'):
-    db.execute("\
-        CREATE TABLE authors (\
-            id SERIAL PRIMARY KEY,\
-            name VARCHAR NOT NULL,\
-            created_at TIMESTAMP,\
-            updated_at TIMESTAMP\
-        )\
-    ")
+def up():
+    if not engine.has_table('authors'):
+        db.execute("\
+            CREATE TABLE authors (\
+                id SERIAL PRIMARY KEY,\
+                name VARCHAR NOT NULL,\
+                created_at TIMESTAMP,\
+                updated_at TIMESTAMP\
+            )\
+        ")
 
-    print("The `authors` table has been created.")
+        print("The `authors` table has been created.")
 
-if not engine.has_table('books'):
-    db.execute("\
-        CREATE TABLE books (\
-            id SERIAL PRIMARY KEY,\
-            isbn VARCHAR NOT NULL,\
-            title VARCHAR NOT NULL,\
-            author_id INTEGER REFERENCES authors(id) NOT NULL,\
-            year INTEGER NOT NULL,\
-            created_at TIMESTAMP,\
-            updated_at TIMESTAMP,\
-            UNIQUE(isbn)\
-        )\
-    ")
+    if not engine.has_table('books'):
+        db.execute("\
+            CREATE TABLE books (\
+                id SERIAL PRIMARY KEY,\
+                isbn VARCHAR NOT NULL,\
+                title VARCHAR NOT NULL,\
+                author_id INTEGER REFERENCES authors(id) NOT NULL,\
+                year INTEGER NOT NULL,\
+                created_at TIMESTAMP,\
+                updated_at TIMESTAMP,\
+                UNIQUE(isbn)\
+            )\
+        ")
 
-    print("The `books` table has been created.")
+        print("The `books` table has been created.")
 
-if not engine.has_table('users'):
-    db.execute("\
-        CREATE TABLE users (\
-            id SERIAL PRIMARY KEY,\
-            username VARCHAR NOT NULL,\
-            password VARCHAR NOT NULL,\
-            forename VARCHAR NOT NULL,\
-            surname VARCHAR,\
-            created_at TIMESTAMP,\
-            updated_at TIMESTAMP,\
-            UNIQUE(username)\
-        )\
-    ")
+    if not engine.has_table('users'):
+        db.execute("\
+            CREATE TABLE users (\
+                id SERIAL PRIMARY KEY,\
+                username VARCHAR NOT NULL,\
+                password VARCHAR NOT NULL,\
+                forename VARCHAR NOT NULL,\
+                surname VARCHAR,\
+                created_at TIMESTAMP,\
+                updated_at TIMESTAMP,\
+                UNIQUE(username)\
+            )\
+        ")
 
-    print("The `users` table has been created.")
+        print("The `users` table has been created.")
 
-if not engine.has_table('reviews'):
-    db.execute("\
-        CREATE TABLE reviews (\
-            id SERIAL PRIMARY KEY,\
-            user_id INTEGER REFERENCES users(id) NOT NULL,\
-            book_id INTEGER REFERENCES books(id) NOT NULL,\
-            rating INTEGER NOT NULL,\
-            comment VARCHAR,\
-            created_at TIMESTAMP,\
-            updated_at TIMESTAMP\
-        )\
-    ")
+    if not engine.has_table('reviews'):
+        db.execute("\
+            CREATE TABLE reviews (\
+                id SERIAL PRIMARY KEY,\
+                user_id INTEGER REFERENCES users(id) NOT NULL,\
+                book_id INTEGER REFERENCES books(id) NOT NULL,\
+                rating INTEGER NOT NULL,\
+                comment VARCHAR,\
+                created_at TIMESTAMP,\
+                updated_at TIMESTAMP\
+            )\
+        ")
 
-    print("The `reviews` table has been created.")
+        print("The `reviews` table has been created.")
 
-db.commit()
+    db.commit()
+
+def down():
+    if engine.has_table('reviews'):
+        db.execute("DROP TABLE reviews")
+        print("The `reviews` table has been dropped.")
+    
+    if engine.has_table('users'):
+        db.execute("DROP TABLE users")
+        print("The `users` table has been dropped.")
+    
+    if engine.has_table('books'):
+        db.execute("DROP TABLE books")
+        print("The `books` table has been dropped.")
+    
+    if engine.has_table('authors'):
+        db.execute("DROP TABLE authors")
+        print("The `authors` table has been dropped.")
+    
+    db.commit()
+
+if __name__ == "__main__":
+    up()
