5f99dfc78f36d996f6dc9f796d49011231526027
JustinByrne <theskaterjesus@gmail.com>
2020-06-13 14:28:59 +0100

    Added load_dotenv to app

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
