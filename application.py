d81de3ad2e65cb5207fa00f9c0989c22e24a9c5d
JustinByrne <theskaterjesus@gmail.com>
2020-06-25 00:26:55 +0100

    Added individual book page

--- application.py
+++ application.py
@@ -40,6 +40,9 @@ def index():
 
 @app.route("/books")
 def books():
+    if session.get('user') is None:
+        return redirect(url_for('login'))
+
     search = request.args.get('search')
 
     if search is None:
@@ -76,6 +79,37 @@ def books():
         {"search": search, "like": ".*" + re.escape(search) + ".*"}).fetchall()
 
     return render_template("books.html", books = books)
+
+@app.route("/books/<isbn>", methods=['GET', 'POST'])
+def book(isbn):
+    if session.get('user') is None:
+        return redirect(url_for('login'))
+    
+    if request.method == 'GET':
+        book = db.execute("SELECT books.id, isbn, title, year, authors.name FROM books JOIN authors ON authors.id = books.author_id WHERE isbn = :isbn", {"isbn": isbn})
+
+        if book.rowcount <= 0:
+            return redirect(url_for('books'))
+        
+        book = book.fetchone()
+        json = requests.get(f"https://www.goodreads.com/book/review_counts.json?isbns={ isbn }&key={ api_key }").json()
+        rating = float(json["books"][0]["average_rating"])
+
+        comments = db.execute('SELECT rating, comment, users.forename FROM reviews JOIN users ON users.id = reviews.user_id WHERE book_id = :book_id ORDER BY reviews.created_at DESC', {'book_id': book.id}).fetchall()
+        average = db.execute("SELECT ROUND(AVG(rating), 2) AS average_rating FROM reviews WHERE book_id = :book_id", {'book_id': book.id}).fetchone()
+        
+        return render_template("book.html", book = book, average = average, rating = rating, comments = comments)
+    
+    if request.method == 'POST':
+        comment = request.form.get('comment-textarea')
+        rating = request.form.get('rating-select')
+        book_id = request.form.get('id')
+        user_id = session['user'].id
+
+        db.execute("INSERT INTO reviews (user_id, book_id, rating, comment, created_at, updated_at) VALUES (:user_id, :book_id, :rating, :comment, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)", {'user_id': user_id, 'book_id': book_id, 'rating': rating, 'comment': comment})
+        db.commit()
+
+        return redirect(url_for('book', isbn=isbn))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
